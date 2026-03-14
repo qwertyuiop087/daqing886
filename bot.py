27a4fd0cebbb56c0e66aa84d4e039477553e0103
qwertyuiop087 <112741406+qwertyuiop087@users.noreply.github.com>
2026-03-14 18:19:00 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,79 +1,66 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot,Dispatcher,types
 from aiogram.utils import executor
-import uuid
 import requests
-from database import add_card, init_db
+import uuid
+from database import init_db,add_card
 
-BOT_TOKEN = "7750611624:AAHlHYVD7aXqQr1aZw3FOWxKO_msw5G0DJU"
-ADMIN_ID = 7793291484
+BOT_TOKEN="你的BOT_TOKEN"
+ADMIN_ID=你的TGID
 
-SHOP_ID = "29681"
-PAY_TOKEN = "8dDikTGgRuxy5ABCsEIKOPb3pL0tr47"
+SHOP_ID="29681"
+CALLBACK="https://daqing886.onrender.com/callback"
 
-CALLBACK_URL = "https://daqing886.onrender.com/callback"
-
-PAY_API = "https://api.okaypay.me/shop/payLink"
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+bot=Bot(token=BOT_TOKEN)
+dp=Dispatcher(bot)
 
 init_db()
 
+def create_pay():
 
-def create_payment(user_id):
+    order=str(uuid.uuid4())
 
-    order_id = str(uuid.uuid4())
-
-    data = {
-        "id": SHOP_ID,
-        "amount": 10,
-        "coin": "USDT",
-        "unique_id": order_id,
-        "name": "卡密商品",
-        "callback_url": CALLBACK_URL
+    data={
+        "id":SHOP_ID,
+        "amount":10,
+        "coin":"USDT",
+        "unique_id":order,
+        "name":"卡密",
+        "callback_url":CALLBACK
     }
 
-    r = requests.post(PAY_API, data=data).json()
+    r=requests.post("https://api.okaypay.me/shop/payLink",data=data).json()
 
     return r["data"]["pay_url"]
 
 
 @dp.message_handler(commands=["start"])
-async def start(message: types.Message):
+async def start(msg:types.Message):
 
-    kb = types.InlineKeyboardMarkup()
-    kb.add(types.InlineKeyboardButton("购买卡密 10元", callback_data="buy"))
+    kb=types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton("购买卡密10元",callback_data="buy"))
 
-    await message.answer("欢迎购买卡密", reply_markup=kb)
+    await msg.answer("欢迎购买",reply_markup=kb)
 
 
-@dp.callback_query_handler(lambda c: c.data == "buy")
-async def buy(call: types.CallbackQuery):
+@dp.callback_query_handler(lambda c:c.data=="buy")
+async def buy(call:types.CallbackQuery):
 
-    pay_url = create_payment(call.from_user.id)
+    url=create_pay()
 
-    await bot.send_message(
-        call.from_user.id,
-        f"点击支付：\n{pay_url}"
-    )
+    await bot.send_message(call.from_user.id,f"支付链接\n{url}")
 
 
 @dp.message_handler(commands=["addcard"])
-async def addcard(message: types.Message):
+async def addcard(msg:types.Message):
 
-    if message.from_user.id != ADMIN_ID:
+    if msg.from_user.id!=ADMIN_ID:
         return
 
-    try:
-        card = message.text.split(" ")[1]
-    except:
-        await message.reply("格式: /addcard 卡密")
-        return
+    card=msg.text.split(" ")[1]
 
     add_card(card)
 
-    await message.reply("卡密已添加")
+    await msg.reply("卡密添加成功")
 
 
-if __name__ == "__main__":
-    executor.start_polling(dp)
+executor.start_polling(dp)
