dec5f5e6eea48a7b05fcd49512ff7852cdf81bf8
qwertyuiop087 <112741406+qwertyuiop087@users.noreply.github.com>
2026-03-14 18:20:03 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,39 +1,33 @@
-from flask import Flask, request
+from flask import Flask,request
 from aiogram import Bot
 import asyncio
 from database import get_card
 
-BOT_TOKEN = "7750611624:AAHlHYVD7aXqQr1aZw3FOWxKO_msw5G0DJU"
+BOT_TOKEN="7750611624:AAGihlmQtN9QQqx_fhZlsKqLh85rS0AoWWY"
 
-bot = Bot(token=BOT_TOKEN)
+bot=Bot(token=BOT_TOKEN)
 
-app = Flask(__name__)
-
+app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot running"
+    return "bot running"
 
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback",methods=["POST"])
 def callback():
 
-    data = request.form
+    status=request.form.get("status")
+    user=request.form.get("pay_user_id")
 
-    status = data.get("status")
-    user_id = data.get("pay_user_id")
+    if status=="1":
 
-    if status == "1":
-
-        card = get_card()
+        card=get_card()
 
         if card:
 
             asyncio.run(
-                bot.send_message(
-                    user_id,
-                    f"支付成功\n\n你的卡密：\n{card}"
-                )
+                bot.send_message(user,f"支付成功\n卡密:{card}")
             )
 
     return "success"
