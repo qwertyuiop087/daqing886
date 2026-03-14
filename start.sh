e7de4cc521e50190afd5fb8de3932dbbb7bcd68d
qwertyuiop087 <112741406+qwertyuiop087@users.noreply.github.com>
2026-03-14 18:49:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-echo "Starting bot..."
-
-python bot.py &
-
 echo "Starting web server..."
-
-gunicorn server:app --bind 0.0.0.0:$PORT --workers 1
+gunicorn server:app --bind 0.0.0.0:$PORT --workers 1 &
+echo "Starting bot..."
+python bot.py
